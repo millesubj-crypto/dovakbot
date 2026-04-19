f4a8d1b45ea38338d7ea784fe30e167058f3182e
millesubj-crypto <millesubj@gmail.com>
2026-04-19 15:49:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-export const CLIENT_ID = process.env.CLIENT_ID;
-export const GUILD_ID = process.env.GUILD_ID;
-export const ADMIN_USER_IDS = process.env.ADMIN_USER_IDS?.split(',') || [];
+export const DISCORD_TOKEN = process.env.MTQyNzYzMzk5NzM0MDc0MTcxNw.GfNdsS.340hAr-UaEJA7wak0uFw4OTabH6kiqVqJMbP_o;
+export const CLIENT_ID = process.env.1427633997340741717;
+export const GUILD_ID = process.env.1469692759823876098;
+export const ADMIN_USER_IDS = process.env.429792107620728833?.split(',') || [];
 export const PORT = process.env.PORT || 10000;
 export const KEEPALIVE_URL = process.env.KEEPALIVE_URL;
 
